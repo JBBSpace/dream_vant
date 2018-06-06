6a0d4eb3e43cb82b83825f7227da80e35b530d86
姬兵兵 <1204680869@qq.com>
2018-06-06 15:17:16 +0800

    图表

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /*
  * @Author: 姬兵兵 
  * @Date: 2018-06-06 10:41:56 
- * @Last Modified by:   姬兵兵 
- * @Last Modified time: 2018-06-06 10:41:56 
+ * @Last Modified by: 姬兵兵
+ * @Last Modified time: 2018-06-06 13:13:12
  */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
@@ -10,8 +10,10 @@
 import Vue from 'vue';
 import Vant from 'vant';
 import 'vant/lib/vant-css/index.css';
+import VCharts from 'v-charts'
 
 Vue.use(Vant);
+Vue.use(VCharts)
 import App from './App'
 import router from './router'
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 import Qurt from '@/views/qurt'
+import Echart from '@/views/echart'
 
 Vue.use(Router)
 
@@ -9,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
+      name: 'home',
       component: Home
     },
     {
